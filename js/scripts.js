dc45cb41f175debc5b9bd5d5685aa50da202fd2e
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2018-12-12 14:48:28 +0200

    adding multiplication function

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,18 @@ var add substract= function(number1, number2) {
 var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
   alert(substract(number1, number2));
+
+var add multiply= function(number1, number2) {
+    return number1 - number2;
+}
+var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(multiply(number1, number2));
+
+var add divide= function(number1, number2) {
+    return number1 - number2;
+}
+var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(devide(number1, number2));
+
