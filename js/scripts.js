4249b6c87766ba59c4dfd4c4c0a664cd7282a32d
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2018-12-12 14:45:56 +0200

    adding substraction function

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,9 @@ var add= function(number1, number2) {
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
   alert(add(number1, number2));
-  
+var add substract= function(number1, number2) {
+    return number1 - number2;
+}
+var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(substract(number1, number2));
